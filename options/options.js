1ba88768fac2e8654c22373251579853fed85835
Crowdbotics <team@crowdbotics.com>
2021-08-06 23:28:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "empty_moon_29588Identifier",
+  url: "https://empty_moon_29588Identifier.botics.co",
+  api: "https://empty_moon_29588Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
